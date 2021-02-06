bf1dd1dfab56eb30b6acade909f1a77a87b6d87d
tadej <tadej@Fata-Mint>
2021-02-06 15:34:58 +0000

    initial commit

---
+++ scottish-munro/src/main/java/si/tadej/kovacic/scottishmunro/ScotishMunroApplication.java
@@ -0,0 +1,13 @@
+package si.tadej.kovacic.scottishmunro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScotishMunroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScotishMunroApplication.class, args);
+	}
+
+}
---
+++ scottish-munro/src/test/java/si/tadej/kovacic/scotishmunro/ScotishMunroApplicationTests.java
@@ -0,0 +1,13 @@
+package si.tadej.kovacic.scotishmunro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScotishMunroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
